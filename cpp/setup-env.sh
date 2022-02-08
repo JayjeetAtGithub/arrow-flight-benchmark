779d492f762ec79f4d69fd8f0389230f85aab4fc
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-08 14:40:53 -0800

    Install arrow and build examples

--- cpp/setup-env.sh
+++ cpp/setup-env.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 set -ex
 
-sudo apt update
-sudo apt install -y -V ca-certificates lsb-release wget
-wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-sudo apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-sudo apt update
-sudo apt install -y -V libarrow-dev # For C++
-sudo apt install -y -V libarrow-glib-dev # For GLib (C)
-sudo apt install -y -V libarrow-dataset-dev # For Apache Arrow Dataset C++
-sudo apt install -y -V libarrow-flight-dev # For Apache Arrow Flight C++
+apt update 
+apt install -y python3 \
+               python3-pip \
+               python3-venv \
+               python3-numpy \
+               cmake \
+               libradospp-dev \
+               rados-objclass-dev \
+               llvm \
+               default-jdk \
+               maven
+
+if [ ! -d "arrow" ]; then
+    git clone https://github.com/apache/arrow
+fi
+
+cd arrow
+git submodule update --init --recursive
+git pull
+
+mkdir -p cpp/release
+cd cpp/release
+
+cmake -DARROW_PARQUET=ON \
+  -DARROW_WITH_SNAPPY=ON \
+  -DARROW_WITH_ZLIB=ON \
+  -DARROW_PYTHON=ON \
+  -DARROW_ORC=ON \
+  -DARROW_COMPUTE=ON \
+  -DARROW_DATASET=ON \
+  -DARROW_CSV=ON \
+  -DARROW_WITH_LZ4=ON \
+  -DARROW_WITH_ZSTD=ON \
+  -DARROW_FLIGHT=ON \
+  -DARROW_BUILD_EXAMPLES=ON \
+  -DPARQUET_BUILD_EXAMPLES=ON \
+  ..
+
+make -j4 install
