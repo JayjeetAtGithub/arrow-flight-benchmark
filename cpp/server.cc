f29a867e48a89ba52feafb7a882854b67bca4a7b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-10 13:05:30 -0800

    Updates

--- cpp/server.cc
+++ cpp/server.cc
@@ -12,29 +12,8 @@
 
 class ParquetStorageService : public arrow::flight::FlightServerBase {
  public:
-  const arrow::flight::ActionType kActionDropDataset{"drop_dataset", "Delete a dataset."};
-
   explicit ParquetStorageService(std::shared_ptr<arrow::fs::FileSystem> root)
       : root_(std::move(root)) {}
-
-  arrow::Status ListFlights(const arrow::flight::ServerCallContext&,
-                            const arrow::flight::Criteria*,
-                            std::unique_ptr<arrow::flight::FlightListing>* listings) {
-    arrow::fs::FileSelector selector;
-    selector.base_dir = "/";
-    ARROW_ASSIGN_OR_RAISE(auto listing, root_->GetFileInfo(selector));
-
-    std::vector<arrow::flight::FlightInfo> flights;
-    for (const auto& file_info : listing) {
-      if (!file_info.IsFile() || file_info.extension() != "parquet") continue;
-      ARROW_ASSIGN_OR_RAISE(auto info, MakeFlightInfo(file_info));
-      flights.push_back(std::move(info));
-    }
-
-    *listings = std::unique_ptr<arrow::flight::FlightListing>(
-        new arrow::flight::SimpleFlightListing(std::move(flights)));
-    return arrow::Status::OK();
-  }
 
   arrow::Status GetFlightInfo(const arrow::flight::ServerCallContext&,
                               const arrow::flight::FlightDescriptor& descriptor,
@@ -43,19 +22,6 @@ class ParquetStorageService : public arrow::flight::FlightServerBase {
     ARROW_ASSIGN_OR_RAISE(auto flight_info, MakeFlightInfo(file_info));
     *info = std::unique_ptr<arrow::flight::FlightInfo>(
         new arrow::flight::FlightInfo(std::move(flight_info)));
-    return arrow::Status::OK();
-  }
-
-  arrow::Status DoPut(const arrow::flight::ServerCallContext&,
-                      std::unique_ptr<arrow::flight::FlightMessageReader> reader,
-                      std::unique_ptr<arrow::flight::FlightMetadataWriter>) {
-    ARROW_ASSIGN_OR_RAISE(auto file_info, FileInfoFromDescriptor(reader->descriptor()));
-    ARROW_ASSIGN_OR_RAISE(auto sink, root_->OpenOutputStream(file_info.path()));
-    std::shared_ptr<arrow::Table> table;
-    ARROW_RETURN_NOT_OK(reader->ReadAll(&table));
-
-    ARROW_RETURN_NOT_OK(parquet::arrow::WriteTable(*table, arrow::default_memory_pool(),
-                                                   sink, /*chunk_size=*/65536));
     return arrow::Status::OK();
   }
 
@@ -69,10 +35,6 @@ class ParquetStorageService : public arrow::flight::FlightServerBase {
 
     std::shared_ptr<arrow::Table> table;
     ARROW_RETURN_NOT_OK(reader->ReadTable(&table));
-    // Note that we can't directly pass TableBatchReader to
-    // RecordBatchStream because TableBatchReader keeps a non-owning
-    // reference to the underlying Table, which would then get freed
-    // when we exit this function
     std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
     arrow::TableBatchReader batch_reader(*table);
     ARROW_RETURN_NOT_OK(batch_reader.ReadAll(&batches));
@@ -83,23 +45,6 @@ class ParquetStorageService : public arrow::flight::FlightServerBase {
         new arrow::flight::RecordBatchStream(owning_reader));
 
     return arrow::Status::OK();
-  }
-
-  arrow::Status ListActions(const arrow::flight::ServerCallContext&,
-                            std::vector<arrow::flight::ActionType>* actions) override {
-    *actions = {kActionDropDataset};
-    return arrow::Status::OK();
-  }
-
-  arrow::Status DoAction(const arrow::flight::ServerCallContext&,
-                         const arrow::flight::Action& action,
-                         std::unique_ptr<arrow::flight::ResultStream>* result) {
-    if (action.type == kActionDropDataset.type) {
-      *result = std::unique_ptr<arrow::flight::ResultStream>(
-          new arrow::flight::SimpleResultStream({}));
-      return DoActionDropDataset(action.body->ToString());
-    }
-    return arrow::Status::NotImplemented("Unknown action type: ", action.type);
   }
 
  private:
@@ -140,21 +85,15 @@ class ParquetStorageService : public arrow::flight::FlightServerBase {
     return root_->GetFileInfo(descriptor.path[0]);
   }
 
-  arrow::Status DoActionDropDataset(const std::string& key) {
-    return root_->DeleteFile(key);
-  }
-
   std::shared_ptr<arrow::fs::FileSystem> root_;
 };
 
 int main() {
   auto fs = std::make_shared<arrow::fs::LocalFileSystem>();
-  fs->CreateDir("./flight_datasets/");
-  fs->DeleteDirContents("./flight_datasets/");
   auto root = std::make_shared<arrow::fs::SubTreeFileSystem>("./flight_datasets/", fs);
 
   arrow::flight::Location server_location;
-  arrow::flight::Location::ForGrpcTcp("0.0.0.0", 33004, &server_location);
+  arrow::flight::Location::ForGrpcTcp("0.0.0.0", 0, &server_location);
 
   arrow::flight::FlightServerOptions options(server_location);
   auto server = std::unique_ptr<arrow::flight::FlightServerBase>(
