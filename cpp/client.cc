5d06bbb3e858e8f33a254b553c3c47d195b07df6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-10 12:41:38 -0800

    Updates

--- cpp/client.cc
+++ cpp/client.cc
@@ -7,7 +7,7 @@
 #include <arrow/io/api.h>
 
 
-arrow::Result<std::shared_ptr<arrow::flight::FlightClient>> ConnectToFlightServer() {
+arrow::Result<std::unique_ptr<arrow::flight::FlightClient>> ConnectToFlightServer() {
   arrow::flight::Location location;
   ARROW_RETURN_NOT_OK(
       arrow::flight::Location::ForGrpcTcp("localhost", 33004, &location));
@@ -19,26 +19,26 @@ arrow::Result<std::shared_ptr<arrow::flight::FlightClient>> ConnectToFlightServe
 }
 
 int main() {
-  auto client = ConnectToFlightServer();
-  // auto descriptor = arrow::flight::FlightDescriptor::Path({"flight_datasets/16MB.uncompressed.parquet"});
-  // std::cout << "Created flight descriptor" << std::endl;
+  auto client = ConnectToFlightServer().ValueOrDie();
+  auto descriptor = arrow::flight::FlightDescriptor::Path({"flight_datasets/16MB.uncompressed.parquet"});
+  std::cout << "Created flight descriptor" << std::endl;
 
-  // std::unique_ptr<arrow::flight::FlightInfo> flight_info;
-  // client->GetFlightInfo(descriptor, &flight_info);
-  // std::cout << flight_info->descriptor().ToString() << std::endl;
+  std::unique_ptr<arrow::flight::FlightInfo> flight_info;
+  client->GetFlightInfo(descriptor, &flight_info);
+  std::cout << flight_info->descriptor().ToString() << std::endl;
 
-  // std::cout << "Got flight info" << std::endl;
+  std::cout << "Got flight info" << std::endl;
 
-  // std::cout << "=== Schema ===" << std::endl;
-  // std::shared_ptr<arrow::Schema> info_schema;
-  // arrow::ipc::DictionaryMemo dictionary_memo;
-  // flight_info->GetSchema(&dictionary_memo, &info_schema);
-  // std::cout << info_schema->ToString() << std::endl;
-  // std::cout << "==============" << std::endl;
+  std::cout << "=== Schema ===" << std::endl;
+  std::shared_ptr<arrow::Schema> info_schema;
+  arrow::ipc::DictionaryMemo dictionary_memo;
+  flight_info->GetSchema(&dictionary_memo, &info_schema);
+  std::cout << info_schema->ToString() << std::endl;
+  std::cout << "==============" << std::endl;
 
-  // std::unique_ptr<arrow::flight::FlightStreamReader> stream;
-  // client->DoGet(flight_info->endpoints()[0].ticket, &stream);
-  // std::shared_ptr<arrow::Table> table;
-  // stream->ReadAll(&table);
-  // std::cout << table->ToString() << std::endl;
+  std::unique_ptr<arrow::flight::FlightStreamReader> stream;
+  client->DoGet(flight_info->endpoints()[0].ticket, &stream);
+  std::shared_ptr<arrow::Table> table;
+  stream->ReadAll(&table);
+  std::cout << table->ToString() << std::endl;
 }
