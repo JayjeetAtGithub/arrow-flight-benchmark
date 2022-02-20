271ffb035361042ed6cb3a30ed85d270479afd47
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-19 17:01:35 -0800

    Add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Benchmarking
+
+1. Deploy data
+```bash
+./common/deploy_data.sh
+```
--- common/deploy_data.sh
+++ common/deploy_data.sh
@@ -3,6 +3,6 @@ set -ex
 
 mkdir -p /mnt/data/flight_dataset
 wget https://skyhook-ucsc.s3.us-west-1.amazonaws.com/16MB.uncompressed.parquet
-for i in {1..625}; do
+for i in {1..100}; do
   cp 16MB.uncompressed.parquet /mnt/data/flight_dataset/16MB.uncompressed.parquet.$i
 done
