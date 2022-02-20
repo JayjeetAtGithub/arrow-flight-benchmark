0bdb9e01b82f401b7d2eb451dc07008d397c4a20
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-19 17:01:35 -0800

    Add readme

--- python/server.py
+++ python/server.py
@@ -22,7 +22,7 @@ class FlightServer(pa.flight.FlightServerBase):
         return self._make_flight_info(descriptor.path[0].decode('utf-8'))
 
     def do_get(self, context, ticket):
-        return pa.flight.RecordBatchStream(self._dataset.to_table())
+        return pa.flight.RecordBatchStream(self._dataset.to_table(use_threads=False))
 
 
 if __name__ == '__main__':
