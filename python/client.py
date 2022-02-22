e389283cca96f98e35827df24d832311b1123674
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-22 09:36:32 -0800

    Update client.py to measure time

--- python/client.py
+++ python/client.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 import pyarrow as pa
 import pyarrow.flight as flight
@@ -10,12 +11,15 @@ if __name__ == "__main__":
     port = int(sys.argv[2])
     dataset_path = str(sys.argv[3])
 
-    location = flight.Location.for_grpc_tcp(host, port)
-    client = pa.flight.connect(location.uri.decode())
-    flight_descriptor = pa.flight.FlightDescriptor.for_path(dataset_path)
-    flight_info = client.get_flight_info(flight_descriptor)
-    print(flight_info.schema)
-
-    reader = client.do_get(flight_info.endpoints[0].ticket)
-    read_table = reader.read_all()
-    print(read_table.to_pandas())
+    for i in range(0, 10):
+        location = flight.Location.for_grpc_tcp(host, port)
+        client = pa.flight.connect(location.uri.decode())
+        flight_descriptor = pa.flight.FlightDescriptor.for_path(dataset_path)
+        flight_info = client.get_flight_info(flight_descriptor)
+        # print(flight_info.schema)
+        s = time.time()
+        reader = client.do_get(flight_info.endpoints[0].ticket)
+        read_table = reader.read_all()
+        e = time.time()
+        # print(read_table.to_pandas())
+        print(e - s)
