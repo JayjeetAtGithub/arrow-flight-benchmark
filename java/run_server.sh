1c9ac4474b7974ba0a450089b187de2c2d85e6b7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-25 23:32:34 -0800

    Add updated scripts

--- java/run_server.sh
+++ java/run_server.sh
@@ -1,1 +1,1 @@
-docker run -it -v $PWD:/workspace -v /mnt:/mnt -w /workspace -p 5055:5055 bench:latest java -jar target/server.jar-jar-with-dependencies.jar -h 0.0.0.0 -p 5055
+docker run -it -v $PWD:/workspace -v /mnt:/mnt -w /workspace -p 5055:5055 bench:latest java -jar /java/target/server.jar-jar-with-dependencies.jar -h 0.0.0.0 -p 5055
