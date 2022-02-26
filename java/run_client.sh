29e96a03a45a56cdcdf288f6baf02b081413240e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-25 23:32:34 -0800

    Add updated scripts

--- java/run_client.sh
+++ java/run_client.sh
@@ -1,1 +1,1 @@
-docker run -it -v $PWD:/workspace -w /workspace -p 5055:5055 bench:latest java -jar target/client.jar-jar-with-dependencies.jar -f /mnt/data/flight_dataset -h 10.10.1.2 -p 5055
+docker run -it -v $PWD:/workspace -w /workspace -p 5055:5055 bench:latest java -jar /java/target/client.jar-jar-with-dependencies.jar -f /mnt/data/flight_dataset -h 10.10.1.2 -p 5055
