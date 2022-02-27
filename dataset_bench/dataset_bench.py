415725a39f556a37c422c3b974cf1625602d3f1c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-26 16:14:10 -0800

    Add python code for dataset bench

--- dataset_bench/dataset_bench.py
+++ dataset_bench/dataset_bench.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     dataset_ = ds.dataset("/mnt/data/flight_dataset", format="parquet")
     for i in range(10):
         s = time.time()
-        table = dataset_.to_table()
+        table = dataset_.to_table(use_threads=False)
         print(table.num_rows)
         e = time.time()
         print(e - s)
