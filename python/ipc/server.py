5dfd2a649306f76d4a6128fa3bdaef3f7b4786f8
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-22 16:29:18 -0700

    fixes

--- python/ipc/server.py
+++ python/ipc/server.py
@@ -22,6 +22,6 @@ if __name__ == '__main__':
                 sink = pa.BufferOutputStream()
                 with pa.ipc.new_stream(sink, batch.schema) as writer:
                     writer.write_batch(batch)
-                conn.sendall(sink.getvalue().to_pybytes())
+                conn.sendall(sink.getvalue().hex)
 
     print("Done sending batches")
