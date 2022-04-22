e65d15769320486ca16cfbe63331d1646eeece1c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-21 22:23:31 -0700

    add client-server ipc without flight

---
+++ python/ipc/client.py
@@ -0,0 +1,29 @@
+import sys
+import time
+
+import pyarrow as pa
+import pyarrow.dataset as ds
+import pyarrow.parquet as pq
+
+import socket
+
+if __name__ == "__main__":
+    host = str(sys.argv[1])
+    port = int(sys.argv[2])
+    dataset_path = str(sys.argv[3])
+
+    for i in range(0, 10):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            st = time.time()
+            while True:
+                data = s.recv(128*1024*1024)
+                if not data:
+                    break
+                else:
+                    with pa.ipc.RecordBatchStreamReader(data) as reader:
+                        batch = reader.read_next_batch()
+                        print(f"Batch size: {batch.num_rows}")
+
+            et = time.time()
+        print(et - st)
---
+++ python/ipc/server.py
@@ -0,0 +1,29 @@
+import sys
+import socket
+import pyarrow as pa
+import pyarrow.dataset as ds
+
+
+if __name__ == '__main__':
+    host = str(sys.argv[1])
+    port = int(sys.argv[2])
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            
+            dataset_ = ds.dataset("/mnt/cephfs/dataset", format="parquet")
+            reader = ds.Scanner.from_dataset(dataset_, use_threads=False).to_reader()
+            num_batches = reader.num_record_batches
+            for i in range(0, num_batches):
+                batch = reader.get_batch(i)
+                sink = pa.BufferOutputStream()
+                with pa.ipc.new_stream(sink, batch.schema) as writer:
+                    writer.write_batch(batch)
+                conn.sendall(sink.getvalue().data)
+                print(f"Sent batch {i}")
+
+    print("Done sending batches")
